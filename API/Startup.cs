6e978c7d9c93b472e5246255afe239752d71fd21
zgl112 <54178625+zgl112@users.noreply.github.com>
2020-04-23 11:40:05 +0100

    ...

---
+++ API/Startup.cs
@@ -0,0 +1,64 @@
+using Application.Activities;
+using MediatR;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Persistance;
+
+namespace API
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<DataContext>(opt =>
+            {
+                opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
+            });
+            services.AddCors(opt =>
+            {
+                opt.AddPolicy("CorsPolicy", policy =>
+                 {
+                     policy.AllowAnyHeader()
+                     .AllowAnyMethod()
+                     .WithOrigins("http://localhost:3000");
+
+                 });
+            });
+            services.AddMediatR(typeof(List.Handler).Assembly);
+            services.AddControllers();
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            // app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+            app.UseCors("CorsPolicy");
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
---
+++ client-app/src/App/Layout/App.tsx
@@ -0,0 +1,28 @@
+import React, { useState, useEffect } from "react";
+import { Header, Icon, List } from "semantic-ui-react";
+import axios from "axios";
+import { IActivity } from "../Models/activity";
+import { NavBar } from "../../Features/nav/NavBar";
+
+const App = () => {
+  const [activities, setActivities] = useState<IActivity[]>([]);
+  useEffect(() => {
+    axios
+      .get<IActivity[]>("http://localhost:5000/api/activities")
+      .then(response => {
+        setActivities(response.data);
+      });
+  }, []);
+  return (
+    <div>
+      <NavBar />
+      <List>
+        {activities.map(activity => (
+          <List.Item key={activity.id}>{activity.title}</List.Item>
+        ))}
+      </List>
+    </div>
+  );
+};
+
+export default App;
