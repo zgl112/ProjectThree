40bf36350414141ad227c287fa664355488ebf2c
StefEmp <stephane.emptage@hotmail.co.uk>
2020-04-24 12:13:59 +0100

    Navbar Edits

--- client-app/src/Features/nav/NavBar.tsx
+++ client-app/src/Features/nav/NavBar.tsx
@@ -1,17 +1,24 @@
 import React from "react";
-import { Menu, Container, Button } from "semantic-ui-react";
+import { Menu, Container, Button} from "semantic-ui-react";
 
 export const NavBar = () => {
   return (
     <Menu fixed="top" inverted>
       <Container>
         <Menu.Item header>
-          <img src="/assets/logo.png" alt="logo" />
-          ReedOnline
+         reed.co.uk
         </Menu.Item>
-        <Menu.Item name="Activities" />
-        <Menu.Item>
-          <Button positive content="Create Activity" />
+        <Menu.Item name="Jobs" />
+        <Menu.Item name="Courses" />
+        <Menu.Item name="Career advice" />
+        <Menu.Item name="Recruiting?Post a job" /> 
+        <Menu.Item position= "right">
+          <Button style={{ backgroundColor:'transparent', border: "1px solid white", borderRadius: "15px", padding: "7px", marginRight: "5px"}}>
+          <p style={{color:"white"}} >Register CV</p></Button>
+          <Button style={{ backgroundColor:'transparent', padding: "3px", marginRight: "5px"}}>
+          <p style={{color:"white"}} >Sign in</p></Button>
+          <Button  style={{ backgroundColor:'transparent', padding: "3px", marginRight: "5px" }} >
+          <p style={{color:"white"}} >Shortlisted jobs</p></Button>
         </Menu.Item>
       </Container>
     </Menu>
