a9a20d299e271e12f86037299dbaf95d3ef0b99a
zgl112 <54178625+zgl112@users.noreply.github.com>
2020-04-30 10:17:56 +0100

    WIP

--- client-app/src/Features/Jobs/Jobs.tsx
+++ client-app/src/Features/Jobs/Jobs.tsx
@@ -1,59 +1,67 @@
 import React from "react";
-import { Image, Segment, Item, Header, Form, Button } from "semantic-ui-react";
-const SearchContainer = {
-  position: "absolute",
-  bottom: "50%",
-  width: "100%",
-  height: "auto",
-  color: "white",
-};
-const SearchCounter = {
-  position: "absolute",
-  textAlign: "Center",
-top: "70px",
-  width: "100%",
-  height: "auto",
-  color: "white",
-};
-const Search = {
-  position: "absolute",
-  bottom: "-70%",
-  right: "13%",
-  width: "75%",
-  height: "auto",
-  color: "white",
-};
-
-
+import {
+  Image,
+  Segment,
+  Item,
+  Header,
+  Form,
+  Button,
+  FormInput,
+  SegmentInline,
+  Divider,
+} from "semantic-ui-react";
 
 export const Jobs = () => {
   return (
     <Segment.Group>
       <Segment basic attached="top" style={{ padding: "0" }}>
-        <Image
-          src= "assets/logoreed.png"
-          fluid
-              /></Segment>
-              <Segment basic style={SearchContainer}>
-              <Item.Group><Item><Item.Content><Header basic style={SearchCounter}><p>Search for your new job</p></Header></Item.Content></Item>
-              </Item.Group>
-              </Segment>
-              <Segment basic style={SearchContainer}>
-               <Form basic style={Search}>
-                 <Form.Group widths = "equal">
-                  <Form.Field fluid  >
-                    <label>What</label>
-                    <input placeholder="e.g. nurse" />
-                  </Form.Field>
-                  <Form.Field fluid >
-                    <label>Where</label>
-                    <input placeholder="location or postcode" />
-                  </Form.Field>
-                  <Form.Button inline type="submit">Submit</Form.Button>
-                </Form.Group>
-                </Form>
-                </Segment>
-                </Segment.Group>
-    
+        <Image src="assets/logoreed.png" fluid />
+        <Form className="searchForm">
+          <Form.Group widths="equal" className="searchFormStyle">
+            <Form.Field>
+              <Form.Input placeholder="e.g. nurse" label="What" />
+            </Form.Field>
+            <Form.Field>
+              <Form.Input placeholder="location or postcode" label="Where" />
+            </Form.Field>
+            <Form.Field>
+              <Button primary>Search</Button>
+            </Form.Field>
+          </Form.Group>
+        </Form>
+      </Segment>
+      {/* <Header basic className="searchCounter">
+        <p className="counterColor">Search for your new job</p>
+      </Header> */}
+      {/* <Segment>
+          <Form className="searchForm">
+        <Form.Group widths="equal" className="searchFormStyle">
+          <Form.Field>
+            <Form.Input placeholder="e.g. nurse" label="What" />
+          </Form.Field>
+          <Form.Field>
+            <Form.Input placeholder="location or postcode" label="Where" />
+          </Form.Field>
+          <Form.Field>
+            <Button primary>Search</Button>
+          </Form.Field>
+        </Form.Group>
+      </Form>
+      </Segment> */}
+      {/* <Form className="searchForm">
+        <Form.Group widths="equal" className="searchFormStyle">
+          <Form.Field>
+            <Form.Input placeholder="e.g. nurse" label="What" />
+          </Form.Field>
+          <Form.Field>
+            <Form.Input placeholder="location or postcode" label="Where" />
+          </Form.Field>
+          <Form.Field>
+            <Button primary>Search</Button>
+          </Form.Field>
+        </Form.Group>
+      </Form> */}
+  
+    </Segment.Group>
   );
 };
